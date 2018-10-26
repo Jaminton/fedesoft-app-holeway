74e6efc6f53c6565901cd9311d84967d2ebef508
jaminton <jamintonrodriguez@outlook.com>
2018-10-25 21:42:21 -0500

    ultimos cambios

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GeneralProvider } from '../../providers/general/general';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    public general:GeneralProvider
+    
+    
+    ) {
+this.general.get('comments').subscribe((data)=>{console.log(data)},(err)=>{console.log("error")
+})
   }
 
 }
